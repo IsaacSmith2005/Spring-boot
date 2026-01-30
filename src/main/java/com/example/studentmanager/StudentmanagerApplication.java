0d286c273fe429915b0574645f7afa325f8bd8bd
IsaacSmith2005 <phuoc106020@donga.edu.vn>
2026-01-30 13:40:30 +0700

    update-database-running-sql-server-onl

--- src/main/java/com/example/studentmanager/StudentmanagerApplication.java
+++ src/main/java/com/example/studentmanager/StudentmanagerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentmanagerApplication {
-
+	
 	public static void main(String[] args) {	
 		SpringApplication.run(StudentmanagerApplication.class, args);
 	}
-}
+}						
